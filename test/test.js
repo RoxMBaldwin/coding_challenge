972c628eeb856ef8607eb3b21a4061cfd750bdb1
Roxanne Baldwin <rox.m.baldwin@gmail.com>
2018-08-06 17:09:13 -0600

    added tests for numberToWords; added break cases

--- test/test.js
+++ test/test.js
@@ -1,9 +1,23 @@
 const assert = require('chai').assert;
-const application = require('../app.js');
+const numberToWords = require('../app.js');
 
-describe('Application', () => {
+describe('numberToWords', () => {
 
-  it('returns desired result', () => {
-    assert.deepEqual(application(arguments), 'desired result')
+  it('takes a number up to one billion', () => {
+    assert.equal(numberToWords(1000000001), 'please enter a less than 1000000000')
   })
+
+  it('takes a number up to the hundreth decimal', () => {
+    assert.equal(numberToWords(99.013), 'please enter a remainer to the hundreth position')
+  })
+
+  it('correctly translates the number value to words', () => {
+    assert.equal(numberToWords(55576683), 'fifty five million five hundred and seventy six thousand six hundred and eighty three dollars')
+    assert.equal(numberToWords(9999182.73), 'nine million nine hundred and ninety nine thousand one hundred and eighty two and 73/100 dollars')
+  })
+
+  // it('returns desired result', () => {
+  //   assert.deepEqual(application(arguments), 'desired result')
+  // })
+
 });
